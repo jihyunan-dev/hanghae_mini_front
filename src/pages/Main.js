4db546a00cc228e38470d3d82c749ab28d9f2b09
An Jihyun <hwiyu25@gmail.com>
2021-07-10 18:28:49 +0900

    [docs] Main 페이지 수정
    Main - 백엔드와 협엽 사항에 따라 메뉴 추천 결과가 게시물이 되도록 변경,댓글 추가
    Result - 결과 페이지 삭제

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../elements/Button";
 
 import Rank from "../components/Rank";
+import { Input } from "../elements";
 
 const Main = () => {
   // 카테고리 정리 필요
@@ -9,6 +10,17 @@ const Main = () => {
   const category2 = ["한식", "중식", "양식", "일식"];
   const category3 = ["혼밥", "친구", "연인", "가족", "모임"];
   const count = ["1가지", "2가지", "3가지"];
+
+  // 가짜 데이터
+  const resultList = [
+    {
+      name: "햄버거",
+      id: 12,
+      img: "https://images.unsplash.com/photo-1598182198871-d3f4ab4fd181?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhhbWJ1cmdlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+    },
+  ];
+
+  const commentList = [];
 
   return (
     <>
@@ -41,12 +53,32 @@ const Main = () => {
         </div>
         <hr />
         <Button text="메뉴 추천받기" />
-        <div>
-          <img src="" alt="" />
-          <p>짜장면</p>
-        </div>
+        {resultList.map((result, idx) => {
+          return (
+            <div key={idx}>
+              <img src={result.img} alt={result.name} />
+            </div>
+          );
+        })}
       </div>
       <Rank />
+      <form>
+        <Input type="text" placeholder="댓글을 입력해주세요" />
+        <Button text={"제출"} />
+      </form>
+      <ul>
+        {commentList.map((comment, idx) => {
+          return (
+            <li>
+              <h6>닉네임</h6>
+              <p>{"댓글내용"}</p>
+              {/* 본인이 쓴 댓글인 경우에만 수정, 삭제 */}
+              <Button text={"수정"} />
+              <Button text={"삭제"} />
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };

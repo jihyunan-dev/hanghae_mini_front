cce855f9b9f0ae5c570f1fbd2121ee0ed73a5d54
An Jihyun <hwiyu25@gmail.com>
2021-07-10 11:05:10 +0900

    [feat] 라우트 연결

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Header from "../components/Header";
 import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Main from "../pages/Main";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
       <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Main} />
+          {/* <Route path="/register" component={Register} /> */}
+          {/* <Route path="/login" component={LOGIN} /> */}
+          {/* <Route path="/result/:menuId" coponent={Result} /> */}
+          {/* <Route path="/upload" component={Upload} /> */}
+          {/* <Route path="/edit/:postId" component={Edit}/> */}
+          {/* <Route path="/posts/userId" component={MyPosts} /> */}
+          {/* <Route component={ErrorPage} /> */}
+        </Switch>
+      </BrowserRouter>
     </>
   );
 };

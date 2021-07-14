6f49d6542170c876a10fb1f930cf90e22ff95a6f
qlemql <busgamer@naver.com>
2021-07-14 16:38:02 +0900

    [docs] server api change

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
+const accessToken = document.cookie.split("=")[1];
+console.log(accessToken);
+
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://52.78.47.49/",
+  headers: {
+    "content-type": "application/json;charset=UTF-8",
+    accept: "application/json,",
+    "X-AUTH-TOKEN": `${accessToken}`,
+  },
 });
 
 export const api_token = axios.create({
-  baseURL: "http://localhost:3001/token",
-  headers: { authorization: "Bearer kpXVCJ9.eygd4MiL....." },
+  baseURL: "http://52.78.47.49/token",
 });

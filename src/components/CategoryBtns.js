afd7c74479237011949794e7104d0e8e7bb9cffa
An Jihyun <hwiyu25@gmail.com>
2021-07-15 16:28:16 +0900

    [feat] 수정 페이지에서 기존에 선택한 카테고리가 선택되어 있도록 기능 추가

--- src/components/CategoryBtns.js
+++ src/components/CategoryBtns.js
@@ -3,16 +3,16 @@ import styled from "styled-components";
 import { Button } from "../elements";
 
 const CategoryBtns = (props) => {
-  const { setCategory } = props;
+  const { setCategory, initial } = props;
 
   // 카테고리 정리 필요
   const btns1 = ["채식", "육식+채식"];
   const btns2 = ["식사", "요리"];
   const btns3 = ["한식", "중식", "일식", "양식", "그 외"];
 
-  const [category1, setCategory1] = useState("채식");
-  const [category2, setCategory2] = useState("식사");
-  const [category3, setCategory3] = useState("한식");
+  const [category1, setCategory1] = useState(initial.first);
+  const [category2, setCategory2] = useState(initial.second);
+  const [category3, setCategory3] = useState(initial.third);
 
   useEffect(
     () => setCategory({ category1, category2, category3 }),
@@ -59,6 +59,14 @@ const CategoryBtns = (props) => {
       </Categorys>
     </Container>
   );
+};
+
+CategoryBtns.defaultProps = {
+  initial: {
+    first: "채식",
+    second: "식사",
+    third: "한식",
+  },
 };
 
 const Container = styled.div`
